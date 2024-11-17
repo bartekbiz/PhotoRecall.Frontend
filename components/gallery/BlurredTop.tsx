f1c476e3df236608362aa122638c53ff69b605af
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2024-11-17 14:50:14 +0100

    Create component for blurred top in Photos view

---
+++ components/gallery/BlurredTop.tsx
@@ -0,0 +1,40 @@
+import {StyleSheet, View} from "react-native";
+import {LinearGradient} from "expo-linear-gradient";
+import {BlurView} from "expo-blur";
+import MaskedView from "@react-native-masked-view/masked-view";
+import * as React from "react";
+
+
+export default function BlurredTop(){
+    return (
+        <MaskedView
+            style={styles.upperBlurContainer}
+            maskElement={
+                <LinearGradient
+                    colors={['rgba(0, 0, 0, 1)', 'rgba(0,0,0, 0.9)',
+                        'rgba(0,0,0, 0.8)', 'transparent']}
+                    style={styles.upperBlur}
+                    start={{ x: 0, y: 0 }}
+                    end={{ x: 0, y: 1 }}/>
+            }
+        >
+            <BlurView style={styles.upperBlur} />
+        </MaskedView>
+    );
+}
+
+const styles = StyleSheet.create({
+    upperBlurContainer: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '16%',
+        zIndex: 1000,
+    },
+    upperBlur: {
+        flex: 1,
+        width: '100%',
+        height: '100%',
+    },
+});
