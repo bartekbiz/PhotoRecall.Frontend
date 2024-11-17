c4af07f59f9f4d8ac2a5bdae4cc8d7de6fcf2088
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2024-11-17 14:50:39 +0100

    Change accent color for light theme to black

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -3,7 +3,7 @@
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
  */
 
-const tintColorLight = '#792db5'; // '#0a7ea4'
+const tintColorLight = '#000000'; // '#0a7ea4'
 const tintColorDark = '#fff';
 
 export const Colors = {
