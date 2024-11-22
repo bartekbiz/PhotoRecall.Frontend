18e2aaa33defd0ba9975d39e09ac8c48bf7cc07b
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2024-11-22 12:07:47 +0100

    Add type DropdownItem

--- constants/Types.ts
+++ constants/Types.ts
@@ -3,3 +3,8 @@ export type NavParams = {
     GalleryHome: undefined;
     GalleryPhotoView: { images: string[]; index: number };
 };
+
+export type DropdownItem = {
+    title: string;
+    value: any;
+}
