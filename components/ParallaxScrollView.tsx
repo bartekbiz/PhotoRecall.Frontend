b2510e4664eb7eaff7f15eba287dd67dedea5885
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2024-11-25 14:40:16 +0100

    Delete unused code

--- components/ParallaxScrollView.tsx
+++ components/ParallaxScrollView.tsx
@@ -1,34 +1,14 @@
-import {StyleSheet, useColorScheme} from 'react-native';
-import Animated, {interpolate, useAnimatedRef, useAnimatedStyle, useScrollViewOffset,} from 'react-native-reanimated';
+import {StyleSheet} from 'react-native';
+import Animated, {useAnimatedRef} from 'react-native-reanimated';
 
 import {ThemedView} from '@/components/ThemedView';
 import {useBottomTabOverflow} from '@/components/ui/TabBarBackground';
 import {ScreenStyles} from "@/constants/Common";
 
-const HEADER_HEIGHT = 250;
-
 
 export default function ParallaxScrollView({children}: any) {
-    const colorScheme = useColorScheme() ?? 'light';
     const scrollRef = useAnimatedRef<Animated.ScrollView>();
-    const scrollOffset = useScrollViewOffset(scrollRef);
     const bottom = useBottomTabOverflow();
-    const headerAnimatedStyle = useAnimatedStyle(() => {
-        return {
-            transform: [
-                {
-                    translateY: interpolate(
-                        scrollOffset.value,
-                        [-HEADER_HEIGHT, 0, HEADER_HEIGHT],
-                        [-HEADER_HEIGHT / 2, 0, HEADER_HEIGHT * 0.75]
-                    ),
-                },
-                {
-                    scale: interpolate(scrollOffset.value, [-HEADER_HEIGHT, 0, HEADER_HEIGHT], [2, 1, 1]),
-                },
-            ],
-        };
-    });
 
     return (
         <ThemedView style={styles.container}>
@@ -47,10 +27,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         paddingTop: 45,
-    },
-    header: {
-        height: HEADER_HEIGHT,
-        overflow: 'hidden',
     },
     content: {
         flex: 1,
