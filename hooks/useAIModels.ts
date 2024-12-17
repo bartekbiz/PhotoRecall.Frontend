018271b850efa1a8cc270ec1688786fd0a3cce78
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2024-12-17 18:12:19 +0100

    Fetch available models from API instead of hardcoding them, store DropdownItemType list instead of string values

---
+++ hooks/useAIModels.ts
@@ -0,0 +1,74 @@
+import {useEffect, useState} from "react";
+import {AppTheme} from "@/constants/Enums";
+import {useStorage} from "@/hooks/useStorage";
+import {DropdownItemType} from "@/constants/Types";
+import {ToTitleCase} from "@/utils/Utils";
+
+export const useAIModels = () => {
+    const storageKey = "models";
+    const [models, setModels] = useState<DropdownItemType[]>([]);
+    const {storeDataJSON, getDataJSON} = useStorage();
+
+    useEffect(() => {
+        getModelsFromAPI()
+            .then(fetched => {
+                let dropdown: DropdownItemType[] = [];
+
+                for (let i = 0; i < fetched.length; i++) {
+                    dropdown.push({
+                        title: fetched[i],
+                        value: fetched[i],
+                        selected: false
+                    })
+                }
+
+                return dropdown;
+            })
+            .then(fetchedDrop => {
+                getModelsFromStorage()
+                    .then(storedDrop => {
+                        let union: DropdownItemType[] = [];
+
+                        for (let i = 0; i < storedDrop.length; i++) {
+                            if (fetchedDrop.find(f => f.value === storedDrop[i].value)) {
+                                union.push(storedDrop[i]);
+                            }
+                        }
+
+                        for (let i = 0; i < fetchedDrop.length; i++) {
+                            if (union.find(f => f.value === fetchedDrop[i].value)) {
+                                continue;
+                            }
+                            union.push(fetchedDrop[i]);
+                        }
+
+                        setModels(union);
+                    })
+            })
+    }, []);
+
+    const getModelsFromAPI = async (): Promise<string[]> => {
+        const requestOptions: RequestInit = {
+            method: "GET",
+            redirect: "follow"
+        };
+
+        return fetch("http://dev.bartekbiz.net:8080/api/Info/GetAllYoloModelsAsync", requestOptions)
+            .then((response) => response.text())
+            .then((result) => JSON.parse(result))
+            .catch((error) => console.error(error));
+    }
+
+    const getModelsFromStorage = async () => {
+        let yoloModels: DropdownItemType[] = await getDataJSON(storageKey);
+
+        return yoloModels !== null ? yoloModels : [];
+    }
+
+    const setModelsStorage = (models: DropdownItemType[]) => {
+        storeDataJSON(storageKey, models)
+            .then(() => setModels(models));
+    }
+
+    return {models, setModels: setModelsStorage}
+}
