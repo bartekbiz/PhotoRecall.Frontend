fd2075411724333dc9871b04b7dcbada9a33fd3a
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2024-12-17 18:26:46 +0100

    Add PhotosContext interface, refresh assets on refresh trigger, add methods for changing GalleryAsset list

--- context/PhotosContext.tsx
+++ context/PhotosContext.tsx
@@ -1,31 +1,67 @@
 import {createContext, useContext, useEffect, useState} from "react";
-import {GalleryAsset} from "@/constants/Types";
 import useUserPhotos from "@/hooks/useUserPhotos";
 import usePhotosAI from "@/hooks/usePhotosAI";
 import {useSettings} from "@/context/SettingsContext";
+import {GalleryAsset} from "@/constants/Types";
 
+interface IPhotosContext {
+    galleryAssets: GalleryAsset[],
+    isPhotosPermission: boolean,
+}
 
-export const PhotosContext = createContext({
-    galleryAssets: [],
-    isPhotosPermission: false,
-});
+export const PhotosContext = createContext<IPhotosContext>({} as IPhotosContext);
 
 export const usePhotos = () => {
     return useContext(PhotosContext);
 };
 
 export const PhotosProvider = ({children}: { children: React.ReactNode; }) => {
-    const {yoloModelsDropdown} = useSettings()
-    const {galleryAssets, setGalleryAssets, isPhotosPermission} = useUserPhotos();
+    const {models, refreshAssets, setRefreshAssets} = useSettings()
+
+    const {refreshGalleryAssets, finishedRefreshing,
+        galleryAssets, setGalleryAssets, isPhotosPermission} = useUserPhotos();
+
     const [selectedModels, setSelectedModels] = useState<string[]>(["yolo11x.pt"])
 
-    usePhotosAI({galleryAssets, setGalleryAssets, selectedModels});
+    usePhotosAI({galleryAssets, updateSingleGalleryAsset, setAssetProcessed, selectedModels, finishedRefreshing});
 
     useEffect(() => {
-        setSelectedModels(yoloModelsDropdown
+        if (!refreshAssets) return;
+        console.log("Assets refreshed!");
+        refreshGalleryAssets();
+        setRefreshAssets(false);
+    }, [refreshAssets]);
+
+    useEffect(() => {
+        setSelectedModels(models
             .filter(f => f.selected)
             .map(m => m.value));
-    }, [yoloModelsDropdown]);
+    }, [models]);
+
+    function updateSingleGalleryAsset(updatedAsset: GalleryAsset) {
+        setGalleryAssets(current => {
+            if (current === undefined) return;
+
+            return current
+                .map(a => {
+                    if (a.localUri === updatedAsset.localUri)
+                        return updatedAsset;
+                    return a;
+                })
+        });
+    }
+
+    function setAssetProcessed(localUri: string, isProcessed: boolean) {
+        setGalleryAssets(current => {
+            if (current === undefined) return;
+
+            return current.map(a => {
+                if (a.localUri === localUri)
+                    a.isProcessed = isProcessed;
+                return a;
+            });
+        })
+    }
 
     const value = {
         galleryAssets,
