52d4a79de2d9b392d6540ab7de37574ae87c1043
Bartosz Bizoń <bartosz.bizon17@gmail.com>
2024-12-17 18:13:33 +0100

    Add util for comparing arrays

--- utils/Utils.ts
+++ utils/Utils.ts
@@ -2,4 +2,13 @@ export function ToTitleCase(str: string): string {
     return str.slice(0, 1).toUpperCase() + str.slice(1).toLowerCase();
 }
 
+export function CompareArrays(arrayA: any[], arrayB: any[], sort?: boolean) {
+    if (sort) {
+        arrayA = arrayA.sort();
+        arrayB = arrayB.sort();
+    }
+
+    return arrayA.length === arrayB.length &&
+    arrayA.every((element, index) => element === arrayB[index]);
+}
 
